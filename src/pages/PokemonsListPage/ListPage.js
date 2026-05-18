962b48540d1c3897a195053e9987f85e48049d77
Giovanni <giovannisouza216@gmail.com>
2026-05-17 22:09:35 -0300

    Ajuste na tela de carregamento

--- src/pages/PokemonsListPage/ListPage.js
+++ src/pages/PokemonsListPage/ListPage.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Header } from "../../components/Header/Header";
 import { Card } from "../../components/PokemonCard/Card";
 import { Loading } from "../../components/Loading/Loading";
@@ -7,7 +7,17 @@ import { ContainerCard, ContainerTexto, Containerzao, H1 } from "./styled";
 
 export const ListPage = () => {
   const context = useContext(GlobalContext);
-  const { pokeList, pokedex, setSelectedPokemon, isLoading } = context;
+  const { pokeList, pokedex, setSelectedPokemon, isLoading: globalIsLoading } = context;
+  const [localLoading, setLocalLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLocalLoading(false);
+    }, 800);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const isLoading = globalIsLoading || localLoading;
 
   const filteredPokeList = pokeList.filter(
     (pokemonInList) =>
